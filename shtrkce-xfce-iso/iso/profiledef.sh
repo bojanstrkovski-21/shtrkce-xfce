36907ed0d6d0bae1d7087c19bf92de9970553cb3
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2023-02-28 20:46:26 +0100

    Update grub, group, and 16 more files...

--- shtrkce-xfce-iso/iso/profiledef.sh
+++ shtrkce-xfce-iso/iso/profiledef.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
+iso_name="shtrkce-xfce"
+iso_label="shtrkce-xfce_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher=" shtrkce Linux <https://github.com/bojanstrkovski-21/>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
@@ -17,6 +17,8 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
